1d9298d8113740c2c8fb1b85006d8eaffb744eae
Saint Jordi <jordiws@gmail.com>
2020-11-24 20:51:32 -0300

    User entity and resource

---
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.educandoweb.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.educandoweb.course.entities.User;
+
+@RestController
+@RequestMapping(value="/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Maiara", "maiara@gmail.com", "+55 55 5555-5555", "bola");
+		return ResponseEntity.ok().body(u);
+	}
+}
