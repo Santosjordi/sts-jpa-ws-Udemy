25ffb9d2c6ff789ef9be93b286b4db49854be812
Saint Jordi <jordiws@gmail.com>
2021-01-23 22:15:40 -0300

    Exception handling - findById

---
+++ src/main/java/com/educandoweb/course/services/exceptions/DatabaseException.java
@@ -0,0 +1,9 @@
+package com.educandoweb.course.services.exceptions;
+
+public class DatabaseException {
+
+	public DatabaseException(String message) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
